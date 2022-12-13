e072e044428a0c416dde45b682899d3c610439d9
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-13 16:44:34 +0530

    bug fixed

--- src/app/search-employee/search-employee.component.ts
+++ src/app/search-employee/search-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search-employee',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class SearchEmployeeComponent {
 
+  constructor(private api:ApiService){}
+
+  empcode=""
+  searchdata:any=[]
+
+  readValue=()=>{
+    let data ={"empcode":this.empcode}
+    console.log(data)
+
+    this.api.searchEmployee(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if(response.length == 0){
+          alert("Invalid employee code")
+        }else{
+          this.searchdata = response;
+        }
+      }
+    )
+  }
 }
